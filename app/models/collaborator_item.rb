6f10ef678c2f4fef51bfbe1dca729f8138f4858e
Hector Correa <hector_correa@brown.edu>
2016-10-19 15:37:49 -0400

    Make faculty collaborator links relative to the site

--- app/models/collaborator_item.rb
+++ app/models/collaborator_item.rb
@@ -1,9 +1,14 @@
 class CollaboratorItem
   attr_accessor :uri, :name, :title
-  
+
   def initialize(uri, name, title)
     @uri = uri
     @name = name
     @title = title
+  end
+
+  def id
+    return "" if @uri == nil
+    uri.split("/").last
   end
 end
