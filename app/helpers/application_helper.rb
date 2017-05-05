79ea876cb95dbd1066ebf72c7ffd5a2ac233c692
Ben Tyler <Ben_Tyler@Brown.edu>
2017-05-05 10:32:09 -0400

    added additional, humanities-related background image (history class reading in Hay’s Willis Reading Room)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
 def randomized_background_image
-  images = ["assets/new-backgrounds/20110620-PAUR-0006.jpg", "assets/new-backgrounds/20131022-PAUR-Aerials-0033.jpg", "assets/new-backgrounds/20140308-PAUR-OpenLabsAndDoors-0007.jpg"]
+  images = ["assets/new-backgrounds/20110620-PAUR-0006.jpg", "assets/new-backgrounds/20131022-PAUR-Aerials-0033.jpg", "assets/new-backgrounds/20140308-PAUR-OpenLabsAndDoors-0007.jpg", "assets/new-backgrounds/20160926_COMM_historyclass09.jpg"]
   images[rand(images.size)]
 end
 
