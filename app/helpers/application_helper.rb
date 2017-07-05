7a11d1e8cf8e8045f4538c303d075af80d7c86ea
Ben Tyler <Ben_Tyler@Brown.edu>
2017-07-05 13:19:52 -0400

    added 3 additional start-page photos (archaeology, Lin sculpture, Saturn project); removed photo of students in Hay

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,9 @@ module ApplicationHelper
     images = ["20110620-PAUR-0006.jpg",
       "20131022-PAUR-Aerials-0033.jpg",
       "20140308-PAUR-OpenLabsAndDoors-0007.jpg",
-      "20160926_COMM_historyclass09.jpg",
+      "20160822_COMM_TessCasswell07.jpg",
+      "20150422_MayaLinTEST-PAUR-0026.jpg",
+      "20131118-PAUR-0005.jpg",
       "20150512-BMED.jpg"]
     "new-backgrounds/" + images[rand(images.size)]
   end
