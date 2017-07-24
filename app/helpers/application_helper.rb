89de7668fec84bf22ca727d9aef577b6be804c8c
Ben Tyler <Ben_Tyler@Brown.edu>
2017-07-24 11:11:42 -0400

    #6 add homepage background photo from 2011 cross-disciplinary Japan session

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,8 @@ module ApplicationHelper
       "20160822_COMM_TessCasswell07.jpg",
       "20150422_MayaLinTEST-PAUR-0026.jpg",
       "20131118-PAUR-0005.jpg",
-      "20150512-BMED.jpg"]
+      "20150512-BMED.jpg",
+      "20110322-COMM-0004.jpg"]
     "new-backgrounds/" + images[rand(images.size)]
   end
 end
