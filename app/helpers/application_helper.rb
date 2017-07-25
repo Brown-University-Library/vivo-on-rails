a8a1064da4a6d96c1d210b47812e0512c8e2e9cc
Ben Tyler <Ben_Tyler@Brown.edu>
2017-07-25 14:20:09 -0400

    add missing ","

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def randomized_background_image
     # TODO: Fetch this from a database
     images = ["20110620-PAUR-0004.jpg",
-      "20130419_WICLACS_0004.jpg"
+      "20130419_WICLACS_0004.jpg",
       "20131022-PAUR-Aerials-0033.jpg",
       "20131118-PAUR-0005.jpg",
       "20140308-PAUR-OpenLabsAndDoors-0007.jpg",
