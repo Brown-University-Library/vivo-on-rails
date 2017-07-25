17df4904361c6ae369ddd98fa8e62c703632006d
Ben Tyler <Ben_Tyler@Brown.edu>
2017-07-25 14:19:32 -0400

    fix list of background image JPEG file names (to match JPEGs in /new-backgrounds)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
   def randomized_background_image
     # TODO: Fetch this from a database
-    images = ["20131022-PAUR-Aerials-0033.jpg",
-      "20140308-PAUR-OpenLabsAndDoors-0007.jpg",
-      "20160822_COMM_TessCasswell07.jpg",
-      "20150422_MayaLinTEST-PAUR-0026.jpg",
+    images = ["20110620-PAUR-0004.jpg",
+      "20130419_WICLACS_0004.jpg"
+      "20131022-PAUR-Aerials-0033.jpg",
       "20131118-PAUR-0005.jpg",
+      "20140308-PAUR-OpenLabsAndDoors-0007.jpg",
+      "20150422_MayaLinTEST-PAUR-0026.jpg",
       "20150512-BMED.jpg",
-      "20110322-COMM-0004.jpg",
-      "20130419_WICLACS_0004.jpg"]
+      "20160822_COMM_TessCasswell07.jpg"]
     "new-backgrounds/" + images[rand(images.size)]
   end
 end
