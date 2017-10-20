4fcbf4d53656b363ed4eca1d64c3f678cf6f6edc
Hector Correa <hector_correa@brown.edu>
2017-10-20 14:51:42 -0400

    Refactored code to handle filtering of PEOPLE and ORGANIZATION records in Solr

--- app/models/search.rb
+++ app/models/search.rb
@@ -12,21 +12,19 @@ class Search
   def search(params)
     extra_fqs = []
     if !record_type_filter?(params)
-      Rails.logger.warn "RECORD_TYPE: added PEOPLE OR ORGANIZATION"
+      # VIVO stores many kind of documents in Solr. We only care about
+      # the ones explicitly marked as PEOPLE or ORGANIZATION. We store
+      # all the information that we need in these two kind of records.
       fq = SolrLite::FilterQuery.new("record_type",["PEOPLE", "ORGANIZATION"])
       extra_fqs = [fq]
-    else
-      Rails.logger.warn "RECORD_TYPE: already filtered"
     end
     params.fl = ["id", "record_type", "json_txt"]
     results = @solr.search(params, extra_fqs)
     results.solr_docs.each do |doc|
       record_type = (doc["record_type"] || []).first
       json_txt = doc["json_txt"].first
-      if record_type != "ORGANIZATION" && record_type != "PEOPLE"
+      if record_type != "PEOPLE" && record_type != "ORGANIZATION"
         # A VIVO type not supported on our new front-end.
-        # TODO: we should either filter them our in our `q` parameter
-        # so that they are not returned at all.
         Rails.logger.warn("Ignored record_type: #{record_type}. #{json_txt}")
         next
       end
