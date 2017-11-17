d747268389601dd9d941d183f3c5362393740e0d
Hector Correa <hector_correa@brown.edu>
2017-11-17 14:57:07 -0500

    Allow a raw Solr HTTP response as an input

--- lib/solr_lite/explainer.rb
+++ lib/solr_lite/explainer.rb
@@ -2,8 +2,8 @@ require "./lib/solr_lite/explain_entry.rb"
 module SolrLite
   class Explainer
     attr_accessor :entries
-    # solr_response_hash is a JSON response from Solr converted to a hash
-    # via JSON.parse()
+
+    # solr_response_hash a Solr HTTP response parsed via JSON.parse()
     def initialize(solr_reponse_hash)
       @explain = solr_reponse_hash.fetch("debug", {}).fetch("explain", [])
       @entries = @explain.map do |ex|
@@ -11,6 +11,12 @@ module SolrLite
         text = ex[1]
         ExplainEntry.new(ex[0], ex[1])
       end
+    end
+
+    # solr_response (string) is the Solr HTTP response from a query
+    def self.from_response(solr_response)
+      hash = JSON.parse(solr_response)
+      Explainer.new(hash)
     end
 
     # Raw string with the explain information for each entry
