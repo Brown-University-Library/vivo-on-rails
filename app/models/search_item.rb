b07dff15c589cc4ce2dcd5d9dadf635dadc9c4d5
Hector Correa <hector_correa@brown.edu>
2018-08-22 16:44:11 -0400

    Better handling of the search results highlights

--- app/models/search_item.rb
+++ app/models/search_item.rb
@@ -36,13 +36,54 @@ class SearchItem
     highlights.keys.each do |key|
       item = {
         field: key,
-        value: highlights[key].join(", ")
+        values: highlights[key]
       }
       parsed << item
     end
     parsed
   rescue
     return []
+  end
+
+  def field_caption(field)
+    case
+    when field == "NAME_T"
+      return "Name"
+    when field == "TITLE_T"
+      return "Title"
+    when field == "DEPARTMENT_T"
+      return "Department"
+    when field == "DEPARMENT_T"
+      return "Department"
+    when field == "EMAIL_S"
+      return "Email"
+    when field == "SHORT_ID_S"
+      return "Id"
+    else
+      return "Text"
+    end
+  end
+
+  def highlights_html()
+    html = ""
+    @highlights.each do |hl|
+      # html += "<p>#{field_caption(hl[:field].upcase)}: "
+      html += "<p>"
+      html += hl[:values].join("...<br/>")
+      html += "</p>"
+    end
+
+    # HTML encode problematic characters so that we can render text safely as HTML
+    html.gsub!("'", '&#39;')
+    html.gsub!('"', '&quote;')
+    html.gsub!('<', '&lt;')
+    html.gsub!('>', '&gt;')
+
+    # Remove the VIVO identifier from the text since it's meaningless to
+    # the user.
+    html.gsub!("Agent Faculty Member Organization or Person at Brown Person", "")
+
+    html
   end
 
 end
