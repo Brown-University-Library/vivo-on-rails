d77c7fd9a9f816b6f116c145bdf4d503d5c3c615
Hector Correa <hector_correa@brown.edu>
2018-09-04 14:11:33 -0400

    Added TODO

--- app/models/search_hit.rb
+++ app/models/search_hit.rb
@@ -3,5 +3,18 @@ class SearchHit
   def initialize(field, value)
     @field = field
     @value = value
+    #
+    # TODO: handle the case where there are more <strong> than </strong>
+    # tags in the value. This could happen because we allow the user
+    # to enter HTML in the field and therefore there is no guarantee that
+    # the Solr snippet will include an exact number of open and close tags.
+    #
+    # As an example see the record for https://vivo.brown.edu/display/ceickhof
+    #
+    # A possible workaround is to use a non-HTML tag as the hl.pre nd hl.post
+    # parameters when querying Solr and add the <strong></strong> tags at the
+    # very end after we have sanitied the value/snippet. The current issue is
+    # compounded because we want to sanitize the HTML except the <strong>
+    # </strong> tags.
   end
 end
