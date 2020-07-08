f5d608ffc2f61dafaa4909d70cd009451e4343b5
Hector Correa <hector_correa@brown.edu>
2020-07-08 15:40:31 -0400

    Temporarily switch to SQLite in RAILS_ENV=production so that we can run in our test server

--- db/migrate/20171016144100_create_book_covers.rb
+++ db/migrate/20171016144100_create_book_covers.rb
@@ -2,7 +2,7 @@
 class CreateBookCovers < ActiveRecord::Migration[4.2]
   def self.up
     options = nil
-    if Rails.env.production?
+    if ENV['DATABASE_ADAPTER'] == "mysql2"
       options = 'DEFAULT CHARSET=utf8'
     end
     create_table(:book_covers, :options => options) do |t|
